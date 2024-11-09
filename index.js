62b3547cf519529d4ccfc6f994f9834e4a44cfcc
Richard Stovall <richard.stovall@inboxhealth.com>
2024-11-09 18:38:46 -0500

    Ember 3 compatibility

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 module.exports = {
-  name: 'ember-select-2',
+  name: "ember-select-2",
 
-  included: function(app) {
+  included: function (app) {
     this._super.included(app);
- 
-    app.import(app.bowerDirectory + '/select2/select2.js');
+
+    app.import("select2/select2.js");
 
     // include assets if there are no options or includeAssets IS NOT false
-    var includeAssets = (app.options.select2 === undefined ||
-      app.options.select2.includeAssets !== false); 
+    var includeAssets =
+      app.options.select2 === undefined ||
+      app.options.select2.includeAssets !== false;
 
     if (includeAssets) {
-      app.import(app.bowerDirectory + '/select2/select2.css');
-      app.import(app.bowerDirectory + '/select2/select2.png', { destDir: 'assets' });
-      app.import(app.bowerDirectory + '/select2/select2x2.png', { destDir: 'assets' });
-      app.import(app.bowerDirectory + '/select2/select2-spinner.gif', { destDir: 'assets' });
+      app.import("select2/select2.css");
+      app.import("select2/select2.png", { destDir: "assets" });
+      app.import("select2/select2x2.png", { destDir: "assets" });
+      app.import("select2/select2-spinner.gif", { destDir: "assets" });
     }
-  }
+  },
 };
